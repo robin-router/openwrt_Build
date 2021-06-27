08cb55ba6a6085a2ccb7fb1dfe30e346280e6032
s1oz <32033912+s1oz@users.noreply.github.com>
2021-06-27 17:49:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -32,7 +32,7 @@ sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-defaul
 #rm -rf package/lean/luci-theme-argon
 
 echo '修改默认主题'
-sed -i 's/luci-theme-bootstrap/luci-theme-Argon_Blue/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' feeds/luci/collections/luci/Makefile
 
 #echo '修改版本号'
 #sed -i "s/OpenWrt /s1oz build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
